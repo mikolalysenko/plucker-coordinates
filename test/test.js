d22b5385d09b00ab62b10e4238a4294c8613f5c5
Mikola Lysenko <mikolalysenko@gmail.com>
2014-04-02 13:57:58 -0500

    adding test shim

---
+++ test/test.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var tape = require("tape")
+var plucker = require("../plucker.js")
+
+tape("plucker-coordinates", function(t) {
+
+  //Verify plucker coordinate module works
+
+  t.end()
+})
